30393a0ebd405d55ef6ac160c34c9a50fc29603a
Asrorbeck <2010288.asrorbek.tursunpulatov@gmail.com>
2024-02-25 10:21:54 +0500

    desktop

---
+++ js/form.js
@@ -0,0 +1,115 @@
+let currentStep = 1;
+
+function updateOptionsAndNext(stepId) {
+  const currentStepSelect = document.getElementById("platform");
+  if (currentStepSelect.value === "none") {
+    alert("Please make a selection before proceeding.");
+    return;
+  }
+
+  updateOptions();
+  nextStep(stepId);
+}
+
+function formatPhoneNumber(input) {
+  let value = input.value.replace(/\D/g, ''); // Remove non-numeric characters
+
+  // Check if the value starts with '7' and remove it
+  if (value.startsWith('7')) {
+    value = value.slice(1);
+  }
+
+  // Add the initial part of the phone number
+  let formattedValue = '+7 (' + value.substring(0, 3) + ')';
+
+  // Use regex to format the remaining part of the phone number
+  formattedValue += value.substring(3).replace(/(\d{3})(\d{2})(\d{1,2})/, '$1-$2-$3');
+
+  input.value = formattedValue;
+}
+
+
+
+
+
+
+function updateOptions() {
+  const platform = document.getElementById("platform").value;
+  const keywordsSelect = document.getElementById("keywords");
+  keywordsSelect.innerHTML = "";
+
+  if (platform === "yandex") {
+    addOption(keywordsSelect, "До 2 000 продвигаемых фраз", 40000);
+    addOption(keywordsSelect, "До 7 000 продвигаемых фраз", 60000);
+    addOption(keywordsSelect, "Более 7 000 продвигаемых фраз", 80000);
+  } else if (platform === "yandex-google") {
+    addOption(keywordsSelect, "До 2 000 продвигаемых фраз", 60000);
+    addOption(keywordsSelect, "До 7 000 продвигаемых фраз", 80000);
+    addOption(keywordsSelect, "Более 7 000 продвигаемых фраз", 100000);
+  }
+}
+
+function addOption(select, text, value) {
+  const option = document.createElement("option");
+  option.className = "form__option";
+  option.text = text;
+  option.value = value;
+  select.add(option);
+}
+
+function nextStep(stepId) {
+  hideAllSteps();
+  showStep(stepId);
+  currentStep++;
+
+  if (currentStep === 5) {
+    // Calculate and display the sum before the 5th step
+    const keywordsValue = parseInt(document.getElementById("keywords").value, 10);
+    const contentStrategyValue = parseInt(document.getElementById("contentStrategy").value, 10);
+    const sum = keywordsValue + contentStrategyValue;
+    const totalCost = document.getElementById("totalCost")
+    totalCost.innerHTML = sum
+  }
+}
+
+function prevStep(stepId) {
+  hideAllSteps();
+  showStep(stepId);
+  currentStep--;
+}
+
+function showStep(stepId) {
+  const step = document.getElementById(stepId);
+  if (step) {
+    step.classList.add("active");
+  }
+}
+
+function hideAllSteps() {
+  const steps = document.querySelectorAll(".form__step");
+  steps.forEach((step) => {
+    step.classList.remove("active");
+  });
+}
+
+function submitForm(event) {
+  event.preventDefault();
+
+  // Retrieve user selections
+  const platform = document.getElementById("platform").value;
+  const keywords = document.getElementById("keywords").value;
+  const contentStrategy = document.getElementById("contentStrategy").value;
+  const gift = document.getElementById("gift").value;
+  const telephone = document.getElementById("tel").value;
+  const name = document.getElementById("name").value;
+  const website = document.getElementById("web").value;
+
+  // Log user selections to the console
+  console.log("Platform:", platform);
+  console.log("Keywords:", keywords);
+  console.log("Content Strategy:", contentStrategy);
+  console.log("Gift:", gift);
+  console.log("Telephone:", telephone);
+  console.log("Name:", name);
+  console.log("Website:", website);
+}
