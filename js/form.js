2b067a455f45319e1ba6b6ebfc57790b7f20365e
Asrorbeck <2010288.asrorbek.tursunpulatov@gmail.com>
2024-02-26 16:33:44 +0500

    telegram

--- js/form.js
+++ js/form.js
@@ -12,26 +12,23 @@ function updateOptionsAndNext(stepId) {
 }
 
 function formatPhoneNumber(input) {
-  let value = input.value.replace(/\D/g, ''); // Remove non-numeric characters
+  let value = input.value.replace(/\D/g, ""); // Remove non-numeric characters
 
   // Check if the value starts with '7' and remove it
-  if (value.startsWith('7')) {
+  if (value.startsWith("7")) {
     value = value.slice(1);
   }
 
   // Add the initial part of the phone number
-  let formattedValue = '+7 (' + value.substring(0, 3) + ')';
+  let formattedValue = "+7 (" + value.substring(0, 3) + ")";
 
   // Use regex to format the remaining part of the phone number
-  formattedValue += value.substring(3).replace(/(\d{3})(\d{2})(\d{1,2})/, '$1-$2-$3');
+  formattedValue += value
+    .substring(3)
+    .replace(/(\d{3})(\d{2})(\d{1,2})/, "$1-$2-$3");
 
   input.value = formattedValue;
 }
-
-
-
-
-
 
 function updateOptions() {
   const platform = document.getElementById("platform").value;
@@ -64,11 +61,17 @@ function nextStep(stepId) {
 
   if (currentStep === 5) {
     // Calculate and display the sum before the 5th step
-    const keywordsValue = parseInt(document.getElementById("keywords").value, 10);
-    const contentStrategyValue = parseInt(document.getElementById("contentStrategy").value, 10);
+    const keywordsValue = parseInt(
+      document.getElementById("keywords").value,
+      10
+    );
+    const contentStrategyValue = parseInt(
+      document.getElementById("contentStrategy").value,
+      10
+    );
     const sum = keywordsValue + contentStrategyValue;
-    const totalCost = document.getElementById("totalCost")
-    totalCost.innerHTML = sum
+    const totalCost = document.getElementById("totalCost");
+    totalCost.innerHTML = sum;
   }
 }
 
@@ -92,10 +95,62 @@ function hideAllSteps() {
   });
 }
 
-function submitForm(event) {
+function sendTelegram3() {
+  var tel = document.getElementById("tel1").value;
+  var name = document.getElementById("name1").value;
+  var web = document.getElementById("web1").value;
+
+  var botToken = "6952866506:AAHwy80JQxTuf2Lq1QTeNmf8h0B_-A0ldxg";
+
+  var chatIds = ["905770018"];
+
+  var message = `Telephone: ${tel}
+
+Name: ${name}
+
+Website: ${web}`;
+
+  var apiUrl = `https://api.telegram.org/bot${botToken}/sendMessage`;
+
+  chatIds.forEach((chatId) => {
+    var data = {
+      chat_id: chatId,
+      text: message,
+    };
+
+    fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(`Message sent`);
+      })
+      .catch((error) => {
+        console.error(`Error sending message:`, error);
+      });
+  });
+
+  document.getElementById("tel1").value = "";
+  document.getElementById("name1").value = "";
+  document.getElementById("web1").value = "";
+
+  return false;
+}
+
+function processForm(event) {
   event.preventDefault();
 
-  // Retrieve user selections
+  sendTelegram3();
+
+  const successModal = document.getElementById("successModal");
+  successModal.style.display = "block";
+}
+
+function sendTelegram3() {
   const platform = document.getElementById("platform").value;
   const keywords = document.getElementById("keywords").value;
   const contentStrategy = document.getElementById("contentStrategy").value;
@@ -104,12 +159,61 @@ function submitForm(event) {
   const name = document.getElementById("name").value;
   const website = document.getElementById("web").value;
 
-  // Log user selections to the console
-  console.log("Platform:", platform);
-  console.log("Keywords:", keywords);
-  console.log("Content Strategy:", contentStrategy);
-  console.log("Gift:", gift);
-  console.log("Telephone:", telephone);
-  console.log("Name:", name);
-  console.log("Website:", website);
+  const keywordsValue = parseInt(document.getElementById("keywords").value, 10);
+  const contentStrategyValue = parseInt(
+    document.getElementById("contentStrategy").value,
+    10
+  );
+  const sum = keywordsValue + contentStrategyValue;
+
+  var botToken = "6952866506:AAHwy80JQxTuf2Lq1QTeNmf8h0B_-A0ldxg";
+
+  var chatIds = ["905770018"];
+
+  var message = `Platforma: ${platform}
+
+keywords: ${keywords}
+
+Website: ${website}
+
+Content Strategy: ${contentStrategy}
+
+gift: ${gift}
+
+telefon: ${telephone}
+
+name: ${name}
+
+totals: ${sum}
+`;
+
+  var apiUrl = `https://api.telegram.org/bot${botToken}/sendMessage`;
+
+  chatIds.forEach((chatId) => {
+    var data = {
+      chat_id: chatId,
+      text: message,
+    };
+
+    fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(`Message sent`);
+      })
+      .catch((error) => {
+        console.error(`Error sending message:`, error);
+      });
+  });
+
+  document.getElementById("tel").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("web").value = "";
+
+  return false;
 }
--- js/index.js
+++ js/index.js
@@ -7,3 +7,153 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the modals and buttons
+  var modal = document.getElementById("myModal");
+  var successModal = document.getElementById("successModal");
+  var openModalBtn = document.getElementById("openModalBtn");
+  var closeModalBtn = document.getElementById("closeModalBtn");
+  var closeSuccessModalBtn = document.getElementById("closeSuccessModalBtn");
+
+  // When the user clicks the button, open the form modal
+  openModalBtn.onclick = function () {
+    modal.style.display = "block";
+  };
+
+  // When the user clicks the close button or outside the modal, close the form modal
+  window.onclick = function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  };
+
+  closeModalBtn.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  // Handle form submission
+
+  var forma = document.getElementById("forma");
+  forma.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    successModal.style.display = "block";
+  });
+
+  window.onclick = function (event) {
+    if (event.target === successModal) {
+      successModal.style.display = "none";
+    }
+  };
+
+  closeSuccessModalBtn.onclick = function () {
+    successModal.style.display = "none";
+  };
+});
+
+function sendTelegram() {
+  var tel = document.getElementById("tel2").value;
+  var name = document.getElementById("name2").value;
+  var web = document.getElementById("web2").value;
+
+  var botToken = "6952866506:AAHwy80JQxTuf2Lq1QTeNmf8h0B_-A0ldxg";
+
+  var chatIds = ["905770018"];
+
+  var message = `Telephone: ${tel}
+
+Name: ${name}
+
+Website: ${web}`;
+
+  var apiUrl = `https://api.telegram.org/bot${botToken}/sendMessage`;
+
+  chatIds.forEach((chatId) => {
+    var data = {
+      chat_id: chatId,
+      text: message,
+    };
+
+    fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(`Message sent`);
+      })
+      .catch((error) => {
+        console.error(`Error sending message:`, error);
+      });
+  });
+
+  document.getElementById("tel2").value = "";
+  document.getElementById("name2").value = "";
+  document.getElementById("web2").value = "";
+
+  return false;
+}
+
+function sendTelegram2() {
+  var tel = document.getElementById("tel1").value;
+  var name = document.getElementById("name1").value;
+  var web = document.getElementById("web1").value;
+
+  var botToken = "6952866506:AAHwy80JQxTuf2Lq1QTeNmf8h0B_-A0ldxg";
+
+  var chatIds = ["905770018"];
+
+  var message = `Telephone: ${tel}
+
+Name: ${name}
+
+Website: ${web}`;
+
+  var apiUrl = `https://api.telegram.org/bot${botToken}/sendMessage`;
+
+  chatIds.forEach((chatId) => {
+    var data = {
+      chat_id: chatId,
+      text: message,
+    };
+
+    fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(`Message sent`);
+      })
+      .catch((error) => {
+        console.error(`Error sending message:`, error);
+      });
+  });
+
+  document.getElementById("tel1").value = "";
+  document.getElementById("name1").value = "";
+  document.getElementById("web1").value = "";
+
+  return false;
+}
+
+var modal = document.getElementById("myModal");
+var successModal = document.getElementById("successModal");
+
+var form = document.getElementById("formModal");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  sendTelegram2();
+
+  successModal.style.display = "block";
+  modal.style.display = "none";
+});
+
+
